d3857bc5c80d0747946329a6d6a4e127adebe3e3
Lucas Alexander <lucasalexander13@hotmail.com>
2021-06-21 18:35:13 -0300

    Added my version of BandNameGenerator

---
+++ 001-MyBandNameGenerator.py
@@ -0,0 +1,9 @@
+#1. I decided to call the user by it's name.
+name = input('Enter your name:')
+print(f'Hi {name}! Welcome to my Band Name Generator.')
+#2. Instead of the city, I decided to use user's favorite color.
+color = input(f'Please, type your favorite color: ')
+#3. And instead of using the user's pet name, i choose to use it's favorite animal.
+animal = input('Nice! Now, type your favorite animal: ')
+#4. The results are pretty the same, but with nicer band names.
+print(f'{name}, your band name should be: {color} {animal}.')
