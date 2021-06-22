a3ca2c111c0beefa6ff2aa9ffa0e91f79b564bc2
Lucas Alexander <lucasalexander13@hotmail.com>
2021-06-22 17:40:25 -0300

    Added TwoDigitNumber

---
+++ 002-TwoDigitNumber.py
@@ -0,0 +1,5 @@
+#1. Write a program that adds the digits in a 2 digit number. 
+first_number = int(input('Enter the first number: '))
+secnd_number = int(input('Type the second number: '))
+
+print(f'The sum is {first_number + secnd_number}.')
