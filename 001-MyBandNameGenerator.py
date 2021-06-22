62af16cd9830c49a29e06eee6725c9d695b4552c
Lucas Alexander <lucasalexander13@hotmail.com>
2021-06-22 18:06:13 -0300

    Maximum line length changed for good practices

--- 001-MyBandNameGenerator.py
+++ 001-MyBandNameGenerator.py
@@ -3,7 +3,7 @@ name = input('Enter your name:')   #bryan
 print(f'Hi {name}! Welcome to my Band Name Generator.')
 #2. Instead of the city, I decided to use user's favorite color.
 color = input('Please, type your favorite color: ')
-#3. And instead of using the user's pet name, i choose to use it's favorite animal.
+#3. Instead of using the user's pet name, I choose to use it's favorite animal.
 animal = input(f'Nice {name}! Now, type your favorite animal: ')
 #4. The results are pretty the same, but with nicer band names.
 print(f'Wow {name}, your band name should be: {color} {animal}.')
