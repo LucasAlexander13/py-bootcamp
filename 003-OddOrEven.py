817b84acf120f9e720222aab89453a241e2e057d
Lucas Alexander <lucasalexander13@hotmail.com>
2021-06-23 15:51:41 -0300

    Added OddOrEven

---
+++ 003-OddOrEven.py
@@ -0,0 +1,10 @@
+'''
+Write a program that works out whether if a 
+given number is an odd or even number.
+'''
+number = float(input('Enter a number: '))
+
+if number % 2 == 0:
+    print('This number is even.')
+else:
+    print('This number is odd.')
