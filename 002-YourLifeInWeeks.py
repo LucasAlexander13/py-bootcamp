17b34822dae1a1956ed042f0336e4e352879dfe5
Lucas Alexander <lucasalexander13@hotmail.com>
2021-06-23 15:27:36 -0300

    Use of list to reduce length

--- 002-YourLifeInWeeks.py
+++ 002-YourLifeInWeeks.py
@@ -1,5 +1,7 @@
-#1. Create a program that tells us how many days, weeks and months
-#we have left if we live until 90 years old.
+'''
+Create a program that tells us how many days, weeks and months
+we have left if we live until 90 years old.
+'''
 age = 90 - int(input('Type your age: '))
-
-print(f'You have {age * 365} days, {age * 52} weeks, and {age * 12} months left.')
+left = [age * 365, age * 52, age * 12]
+print(f'You have {left[0]} days, {left[1]} weeks, and {left[2]} months left.')
