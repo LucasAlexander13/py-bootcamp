471f4de316f6cea69ba729a7e731aa14fad29095
Lucas Alexander <lucasalexander13@hotmail.com>
2021-06-23 15:30:11 -0300

    Updated DataTypes

--- 002-DataTypes.py
+++ 002-DataTypes.py
@@ -1,5 +1,5 @@
 #1. write a program that get a two digit number and returns it's sum.
 number = int(input('Type a two digit number: '))
-sum = (number % 10) + (number // 10)
+sum = (number // 10) + (number % 10)
 
 print(f'The sum is {sum}.')
