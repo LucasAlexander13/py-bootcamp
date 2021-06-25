d7f22f5d63e7857a0e88a6490f04bb11f594442f
Lucas Alexander <lucasalexander13@hotmail.com>
2021-06-25 09:29:37 -0300

    Updated TreasureMap

--- 004-TreasureMap.py
+++ 004-TreasureMap.py
@@ -3,17 +3,26 @@ Your job is to write a program that allows you to mark a square on the map
 using a two-digit system. The first digit is the vertical column number 
 and the second digit is the horizontal row number.
 '''
-column1 = ['_', '_', '_']
-column2 = ['_', '_', '_']
-column3 = ['_', '_', '_']
-map = [column1, column2, column3]
 
-index = 0
-print('     1    2    3')
-for column in map:
-    print(f'{(index + 1):^3}{map[index]}')
-    index += 1
+def call(list):
+    index = 0
+    print('     1    2    3')
+
+    for row in list:
+        print(f'{(index + 1):^3}{list[index]}')
+        index += 1
+
+row1 = ['_', '_', '_']
+row2 = ['_', '_', '_']
+row3 = ['_', '_', '_']
+
+map = [row1, row2, row3]
+call(map)
 
 treasure = int(input('Where do you want to hide the treasure: '))
 
+row = (treasure // 10) 
+column = (treasure % 10) 
+map[row - 1][column - 1] = 'X'
 
+call(map)
