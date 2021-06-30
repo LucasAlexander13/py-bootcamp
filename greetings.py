ae5b32888ee328ca8ffdc61beec30bc53ecf3ef0
Lucas Alexander <lucasalexander13@hotmail.com>
2021-06-30 15:40:10 -0300

    Added greetings

---
+++ greetings.py
@@ -0,0 +1,11 @@
+'''
+Write a program that call a function called greet and 
+that prints three lines of greetings to the user.
+'''
+
+def greet():
+    print('Welcome to my greeting program.')
+    name = input('Please, tell me your name: ')
+    print(f'Welcome {name}! It\'s nice to see you here.')
+
+greet()
