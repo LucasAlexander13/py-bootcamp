caaa4e4771efad9f7dc8da4e397c6de6ec79ce65
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-03 23:36:46 -0300

    Modified function

--- day_008/prime_number.py
+++ day_008/prime_number.py
@@ -3,10 +3,11 @@ You need to write a function that checks whether if the
 number passed into it is a prime number or not.
 '''
 
-def prime(number):
+def prime():
+    number = int(input('Enter a number: '))
     for i in range(2, number):
         if number % i == 0:
-            return False
-    return True
+            return print('It is not a prime number.') 
+    return print('It is a prime number.')
 
-print(prime(int(input('Enter a number: '))))
+prime()
