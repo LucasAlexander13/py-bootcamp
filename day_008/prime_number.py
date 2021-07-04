954a1d02fb36139218d0e0682e4947f93ea3906c
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-03 23:42:19 -0300

    Added a while loop

--- day_008/prime_number.py
+++ day_008/prime_number.py
@@ -3,11 +3,12 @@ You need to write a function that checks whether if the
 number passed into it is a prime number or not.
 '''
 
-def prime():
-    number = int(input('Enter a number: '))
+def prime(number):
     for i in range(2, number):
         if number % i == 0:
             return print('It is not a prime number.') 
     return print('It is a prime number.')
 
-prime()
+while True:
+    number = int(input('\nEnter a number: '))
+    prime(number)
