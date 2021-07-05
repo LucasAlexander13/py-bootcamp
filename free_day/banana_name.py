2ac7eb1ace2894600a9ca690e740801ac737a9c4
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-05 13:40:08 -0300

    Title function applied to name

--- free_day/banana_name.py
+++ free_day/banana_name.py
@@ -5,7 +5,7 @@ name = input('Enter your full name: ').split()
 first_name = name[0]
 last_name = name[-1]
 
-name = first_name + ' Banana ' + last_name
+name = (first_name + ' Banana ' + last_name).title()
 
 print(banana)
 print(f'Welcome {name}!')
