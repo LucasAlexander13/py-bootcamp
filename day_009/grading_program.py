464c2d9c9338bd2684f529a4e82ab3268aa65408
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-06 17:50:34 -0300

    Added grading_program

---
+++ day_009/grading_program.py
@@ -0,0 +1,21 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+#Don't change the code above
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.
+student_grades["Harry"] = "Exceeds Expectations"
+student_grades["Ron"] = "Acceptable"
+student_grades["Hermione"] = "Outstanding"
+student_grades["Draco"] = "Acceptable"
+student_grades["Neville"] = "Fail"
+
+#Don't change the code below
+print(student_grades)
