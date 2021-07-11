02df75094d19276384418b93b4377e75867d83c1
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-11 13:36:17 -0300

    month_days list changed to dictionary

--- day_010/days_in_month.py
+++ day_010/days_in_month.py
@@ -5,15 +5,28 @@ def is_leap(year):
         return False
 
 def days_in_month(year, month):
-    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    month_days = {
+        'January': 31,
+        'February': 28,
+        'March': 31,
+        'April': 30,
+        'May': 31,
+        'June': 30,
+        'July': 31,
+        'August': 31,
+        'September': 30,
+        'October': 31,
+        'November': 30,
+        'December': 31
+        }
     
     if is_leap(year):
-        month_days[1] = 29
+        month_days['February'] = 29
     
-    month = month_days[month - 1]
+    month = month_days[month]
     return month
 
 year = int(input('Enter a year: '))
-month = int(input('Enter a month: '))
+month = input('Enter a month: ').title()
 
 print(days_in_month(year, month))
