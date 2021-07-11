5a419754ab44e48123dc7acdbb3e58e04833f0c5
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-11 13:49:37 -0300

    Added DocStrings

--- day_010/days_in_month.py
+++ day_010/days_in_month.py
@@ -1,10 +1,18 @@
 def is_leap(year):
+    '''
+    Return True if the given number is a leap 
+    year and False if it is not a leap year
+    '''
     if (year % 400 == 0) or (year % 4 == 0 and not year % 100 == 0):
         return True
     else:
         return False
 
+
 def days_in_month(year, month):
+    '''
+    Return how much days a given month has in a given year.
+    '''
     month_days = {
         'January': 31,
         'February': 28,
