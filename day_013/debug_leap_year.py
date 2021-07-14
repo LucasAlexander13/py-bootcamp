96657449f247a0a043938a22cffee3791725fc99
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-14 15:51:52 -0300

    Added original code

--- day_013/debug_leap_year.py
+++ day_013/debug_leap_year.py
@@ -1,3 +1,17 @@
+#year = input("Which year do you want to check?")
+#
+#if year % 4 == 0:
+#  if year % 100 == 0:
+#    if year % 400 == 0:
+#      print("Leap year.")
+#    else:
+#      print("Not leap year.")
+#  else:
+#    print("Leap year.")
+#else:
+#  print("Not leap year.")
+
+
 year = input("Which year do you want to check?: ")
 
 if year % 4 == 0:
