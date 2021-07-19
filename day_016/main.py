26b1d94fa9893062f20681a96d12c6e5562ce42b
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-19 15:56:55 -0300

    More readable variables

--- day_016/main.py
+++ day_016/main.py
@@ -6,11 +6,6 @@ coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 menu = Menu()
 
-def order(coffee):
-    if coffee_maker.is_resource_sufficient(coffee):
-        if money_machine.make_payment(coffee.cost):
-            coffee_maker.make_coffee(coffee)
-
 while True:
     request = input(f'What would you like? ({menu.get_items()}): ')
 
@@ -18,11 +13,14 @@ while True:
         coffee_maker.report()
         money_machine.report()
 
-    elif request in ['latte', 'espresso', 'cappuccino']:
-        order(menu.find_drink(request))
-    
     elif request == 'refill':
         coffee_maker.resources = {'water': 300, 'milk': 200, 'coffee': 100}
 
     else:
-        print('Invalid input.')
+        try:
+            coffee = menu.find_drink(request)
+            if coffee_maker.is_resource_sufficient(coffee):
+                if money_machine.make_payment(coffee.cost):
+                    coffee_maker.make_coffee(coffee)
+        except:
+            print('Something goes wrong.')
