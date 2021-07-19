b6a5189e0216c8f0e8ea462a9dd5907c96b390dc
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-19 14:54:17 -0300

    OOP coffee machine finished

--- day_016/main.py
+++ day_016/main.py
@@ -11,7 +11,6 @@ espresso = MenuItem('espresso', 50, 0, 18, 1.50)
 latte = MenuItem('latte', 200, 150, 24, 2.50)
 cappuccino = MenuItem('cappuccino', 250, 100, 24, 3.00)
 
-
 while ordering:
     request = input(f'What would you like? ({menu.get_items()}): ')
 
@@ -21,16 +20,17 @@ while ordering:
     else:
         if request == 'espresso':
             if coffee_maker.is_resource_sufficient(espresso):
-                print('ahoy')
-            else:
-                print('nope')
+                if money_machine.make_payment(1.50):
+                    coffee_maker.make_coffee(espresso)
+
         elif request == 'latte':
             if coffee_maker.is_resource_sufficient(latte):
-                print('ahoy')
-            else:
-                print('nope')
+                if money_machine.make_payment(2.50):
+                    coffee_maker.make_coffee(latte)
+
         elif request == 'cappuccino':
             if coffee_maker.is_resource_sufficient(cappuccino):
-                print('ahoy')
-            else:
-                print('nope')
+                if money_machine.make_payment(3.00):
+                    coffee_maker.make_coffee(cappuccino)
+        else:
+            print('Invalid input.')
