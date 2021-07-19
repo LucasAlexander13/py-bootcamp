9a13a51a5780868fcbdf0f2870af65c181e05174
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-19 15:06:07 -0300

    Added an order function and a dictionary

--- day_016/main.py
+++ day_016/main.py
@@ -2,7 +2,6 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-ordering = True
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 menu = Menu()
@@ -10,27 +9,23 @@ menu = Menu()
 espresso = MenuItem('espresso', 50, 0, 18, 1.50)
 latte = MenuItem('latte', 200, 150, 24, 2.50)
 cappuccino = MenuItem('cappuccino', 250, 100, 24, 3.00)
+menu = {'espresso': espresso, 'latte': latte, 'cappuccino': cappuccino}
 
-while ordering:
+def order(coffee):
+    coffee = menu[coffee]
+    if coffee_maker.is_resource_sufficient(coffee):
+        if money_machine.make_payment(coffee.cost):
+            coffee_maker.make_coffee(coffee)
+
+while True:
     request = input(f'What would you like? ({menu.get_items()}): ')
 
     if request == 'report':
         coffee_maker.report()
         money_machine.report()
+
+    elif request in menu:
+        order(request)
+
     else:
-        if request == 'espresso':
-            if coffee_maker.is_resource_sufficient(espresso):
-                if money_machine.make_payment(1.50):
-                    coffee_maker.make_coffee(espresso)
-
-        elif request == 'latte':
-            if coffee_maker.is_resource_sufficient(latte):
-                if money_machine.make_payment(2.50):
-                    coffee_maker.make_coffee(latte)
-
-        elif request == 'cappuccino':
-            if coffee_maker.is_resource_sufficient(cappuccino):
-                if money_machine.make_payment(3.00):
-                    coffee_maker.make_coffee(cappuccino)
-        else:
-            print('Invalid input.')
+        print('Invalid input.')
