eee7939182c1b109389bffccccfc6fb3f0b8cd60
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-27 21:44:13 -0300

    Added new functions

--- day_017/main.py
+++ day_017/main.py
@@ -2,12 +2,10 @@ from question_model import *
 from data import question_data
 
 question_bank = []
+question_list = get_questions()
 
 for question in question_data:
     text = question['text']
     answer = question['answer']
     question = Question(text, answer)
     question_bank.append(question)
-
-print(question_bank)
-
--- day_017/question_model.py
+++ day_017/question_model.py
@@ -1,5 +1,21 @@
+from random import randint
+
+def get_questions():
+    questions = []
+
+    while len(questions) < 4:
+        random = randint(0, 11)
+        if random not in questions:
+            questions.append(random)
+    return questions
+
 class Question():
 
     def __init__(self, text,  answer):
         self.text  = text
         self.answer = answer
+
+    def compare(self):
+        print(self.text)
+        if  input('True or False?: ').title() ==  self.answer:
+            print('ok')
