11902d7501e27edf135993269d73a55605f0c64d
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-28 14:36:57 -0300

    Code refactoring

--- day_017/question_model.py
+++ day_017/question_model.py
@@ -1,26 +1,6 @@
-from random import randint
-
-def get_questions():
-    questions = []
-
-    while len(questions) < 4:
-        random = randint(0, 11)
-        if random not in questions:
-            questions.append(random)
-    return questions
-
 class Question():
 
     def __init__(self, text,  answer):
         self.text  = text
         self.answer = answer
 
-    def compare(self):
-        if  input('True or False?: ').title() ==  self.answer:
-            print('\nYou got it right!')
-            print(f'The correct answer was: {self.answer}')
-            return True
-        else:
-            print('\nThat\'s wrong.')
-            print(f'The correct answer was: {self.answer}')
-            return False
