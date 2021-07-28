78a8768757b3267298abee2b4e4382098c8da250
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-28 14:50:03 -0300

    Added main function

--- day_017/main.py
+++ day_017/main.py
@@ -1,5 +1,6 @@
-from question_model import *
+from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -8,3 +9,11 @@ for question in question_data:
     answer = question['answer']
     question = Question(text, answer)
     question_bank.append(question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print('You\'ve completed the quiz')
+print(f'Your final score was: {quiz.score}/{quiz.question_number}')
