f04f4a9d7d723098eadc5ae886c5f452086e8080
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-29 17:47:49 -0300

    Changed directions to list

--- day_018/random_walk.py
+++ day_018/random_walk.py
@@ -7,17 +7,10 @@ tim.shape('circle')
 tim.pen(pensize=10)
 tim.speed('fastest')
 
-direction = {
-    'right': 90,
-    'forward': 0,
-    'back': 180,
-    'left': -90
-}
+direction = [0, 90, 180, 270]
 color = ['black','orange', 'dark orange', 'firebrick', 'dark red', 'crimson', 'medium violet red', 'dark magenta', 'medium orchid', 'medium purple', 'indigo', 'slate blue']
 
 while True:
-    random_dir = choice(list(direction))
-    
     tim.color(choice(color))
-    tim.right(direction[random_dir])
+    tim.setheading(choice(direction))
     tim.forward(30)
