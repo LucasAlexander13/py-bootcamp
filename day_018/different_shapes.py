d10c6cd934ca73219a17959deccd2f2d5cdd2b76
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-29 17:15:44 -0300

    Added color to tim

--- day_018/different_shapes.py
+++ day_018/different_shapes.py
@@ -4,16 +4,15 @@ tim = Turtle()
 screen = Screen()
 tim.shape('turtle')
 tim.speed('slow')
-tim.color('dark green', 'green')
 
-shape_side = 3
-shape_angle = 360 / shape_side
+color = ['black', 'black', 'black', 'orange', 'dark orange', 'firebrick', 'dark red', 'crimson', 'medium violet red', 'dark magenta', 'medium orchid', 'medium purple', 'indigo', 'slate blue']
 
-for shapes in range(7):
+def draw_shape(shape_side):
+    angle = 360 / shape_side
+    for side in range(shape_side):
+        tim.forward(100)
+        tim.right(angle)
 
-    for sides in range(shape_side):
-        tim.forward(50)
-        tim.right(shape_angle)
-
-    shape_side += 1
-    shape_angle = 360 / shape_side
+for shape in range(3, 10):
+    tim.color(color[shape])
+    draw_shape(shape)
