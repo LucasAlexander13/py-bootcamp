36d7af2c26409623b885db3e54eafcfacd16f574
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-29 15:43:16 -0300

    Chaged tim speed

--- day_018/draw_square.py
+++ day_018/draw_square.py
@@ -1,11 +1,10 @@
 from turtle import Screen, Turtle
-from time import sleep
 
 tim = Turtle()
 screen = Screen()
 tim.shape('turtle')
+tim.speed('slow')
 
 for i in range(4):
-    sleep(1)
     tim.forward(75)
     tim.left(90)
