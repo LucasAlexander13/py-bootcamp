e8b84fdf6ae241db355a611d68e9167ae35624ce
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-01 10:35:37 -0300

    Added a clear function

--- day_019/etch_a_sketch.py
+++ day_019/etch_a_sketch.py
@@ -15,10 +15,14 @@ def turn_right(): #d key
 def turn_left(): #a key
     tim.left(15)
 
+def clear_screen(): #c key
+    tim.reset()
+
 screen.listen()
 screen.onkey(key='w', fun=move_forward)
 screen.onkey(key='s', fun=move_backward)
 screen.onkey(key='d', fun=turn_right)
 screen.onkey(key='a', fun=turn_left)
+screen.onkey(key='c', fun=clear_screen)
 
 screen.exitonclick()
