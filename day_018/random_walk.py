097dd01a974aa6b13c527b5858134394294591a4
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-01 10:55:24 -0300

    User wasd input control added

--- day_018/random_walk.py
+++ day_018/random_walk.py
@@ -1,18 +1,52 @@
-from turtle import Screen, Turtle
+from turtle import Screen, Turtle, exitonclick
 from random import choice
+
+def move_up():
+    tim.color(choice(color))
+    tim.setheading(90)
+    tim.forward(30)
+
+def move_left():
+    tim.color(choice(color))
+    tim.setheading(180)
+    tim.forward(30)
+
+def move_down():
+    tim.color(choice(color))
+    tim.setheading(270)
+    tim.forward(30)
+
+def move_right():
+    tim.color(choice(color))
+    tim.setheading(0)
+    tim.forward(30)
+
+def clear():
+    tim.reset()
+    tim.pen(pensize=10)
+    tim.speed('fastest')
 
 tim = Turtle()
 screen = Screen()
 tim.shape('circle')
 tim.pen(pensize=10)
-tim.speed('fast')
+tim.speed('fastest')
 
 direction = [0, 90, 180, 270]
 color = ['black','orange', 'dark orange', 'firebrick', 'dark red', 'crimson', 'medium violet red', 'dark magenta', 'medium orchid', 'medium purple', 'indigo', 'slate blue']
 
-while True:
-    tim.color(choice(color))
-    tim.setheading(choice(direction))
-    tim.forward(30)
+screen.listen()
+screen.onkey(move_up, 'w')
+screen.onkey(move_left, 'a')
+screen.onkey(move_down, 's')
+screen.onkey(move_right, 'd')
+screen.onkey(clear, 'c')
+screen.onkey(screen.bye, 'x')
 
-    screen.onclick(tim.goto)
+while True:
+    try:
+        tim.color(choice(color))
+        tim.setheading(choice(direction))
+        tim.forward(30)
+    except:
+        break
