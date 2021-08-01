af1fc4ef27ec1a284834f4be02c81377d29c124d
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-01 10:31:14 -0300

    Added event listener

--- day_019/etch_a_sketch.py
+++ day_019/etch_a_sketch.py
@@ -10,8 +10,15 @@ def move_backward(): #s key
     tim.backward(15)
 
 def turn_right(): #d key
-    tim.right(5)
+    tim.right(15)
 
 def turn_left(): #a key
-    tim.left(5)
+    tim.left(15)
 
+screen.listen()
+screen.onkey(key='w', fun=move_forward)
+screen.onkey(key='s', fun=move_backward)
+screen.onkey(key='d', fun=turn_right)
+screen.onkey(key='a', fun=turn_left)
+
+screen.exitonclick()
