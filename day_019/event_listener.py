f278800c2391c2fef0b0efcff74e53b4a4445f63
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-01 10:19:38 -0300

    Added all move functions

--- day_019/event_listener.py
+++ day_019/event_listener.py
@@ -3,9 +3,26 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen  = Screen()
 
-def move_forward():
-    tim.forward(10)
+def move_up():
+    tim.setheading(90)
+    tim.forward(15)
+
+def move_left():
+    tim.setheading(180)
+    tim.forward(15)
+
+def move_down():
+    tim.setheading(270)
+    tim.forward(15)
+
+def move_right():
+    tim.setheading(0)
+    tim.forward(15)
 
 screen.listen()
-screen.onkey(key='d', fun=move_forward)
+screen.onkey(key='w', fun=move_up)
+screen.onkey(key='a', fun=move_left)
+screen.onkey(key='s', fun=move_down)
+screen.onkey(key='d', fun=move_right)
+
 screen.exitonclick()
