2681fd514405254a2b0e40ea4b4b125a4a03af70
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-02 12:00:30 -0300

    Guess and loop added

--- day_019/multiplayer.py
+++ day_019/multiplayer.py
@@ -1,5 +1,6 @@
 from color import color
 from turtle import Turtle, Screen
+from random import choice
 
 screen = Screen()
 screen.setup(500, 400)
@@ -34,5 +35,21 @@ bug.color(color[9])
 bug.penup()
 bug.setposition(-230, -60)
 
+turtles = [sam, deb, tim, dom, bug]
 guess = screen.textinput('Make your bet',  'Which turtle will win the run?: ')
+
+while True:
+
+    chosen_turtle = choice(turtles)
+    chosen_turtle.forward(10)
+    if chosen_turtle.xcor() ==  180:
+        chosen_turtle.write('I win', align='left')
+        if chosen_turtle.color() == guess:
+            chosen_turtle.write('And you win too', align='left')
+        else:
+            chosen_turtle.write('But you lose', align='left')
+        break
+
+
+
 screen.exitonclick()
