4d981304ddc525fa0f3c9f1f3e6d9615d7c1e23f
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-02 12:14:21 -0300

    Turtle race added

--- day_019/turtle_race.py
+++ day_019/turtle_race.py
@@ -1,5 +1,6 @@
 from my_turtles import turtles, screen
 from random import choice
+from time import sleep
 
 guess = screen.textinput('Make your bet',  'Which turtle will win the run?: ')
 
@@ -8,7 +9,9 @@ while True:
     chosen_turtle = choice(turtles)
     chosen_turtle.forward(10)
     if chosen_turtle.xcor() == 180:
+        sleep(1)
         chosen_turtle.write('I win', align='left')
+        sleep(2)
         if chosen_turtle.color() == guess:
             chosen_turtle.write('And you win too', align='left')
         else:
