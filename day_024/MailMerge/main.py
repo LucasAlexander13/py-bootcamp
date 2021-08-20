03850ec3af8d3e861b066c5c3e77a0aa3c9414b4
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-19 21:56:59 -0300

    Starting_letter file reading

--- day_024/MailMerge/main.py
+++ day_024/MailMerge/main.py
@@ -3,3 +3,6 @@ with open('./Input/Names/invited_names.txt', 'r') as file:
     for name in file.readlines():
         name_list.append(name[:(len(name)-1)])
     file.close()    
+
+with open('./Input/Letters/starting_letter.txt', 'r') as file:
+    letter = file.read()
